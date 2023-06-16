7df84b9cc1fa4b29cd27704ec175400f590206db
wrikych <59848247+wrikych@users.noreply.github.com>
2023-06-16 17:15:42 -0500

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,7 @@ const App = () => {
         <Nav />
         <About />
         <Experience />
-        <Services />
         <Portfolio />
-        <Testimonials />
         <Contact />
         <Footer />
     </>
